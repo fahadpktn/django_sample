3f333c0b43d555835425d119f9c09dace4c95db8
fahadpktn <fahadpktn@gmail.com>
2022-06-11 20:10:14 +0530

    login form worked

--- sample/login/urls.py
+++ sample/login/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="templates/index")
 ]
 
 
--- sample/sample/urls.py
+++ sample/sample/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('login.urls')),
+    path('admin/', admin.site.urls),
+
 
 ]
