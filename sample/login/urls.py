e713f04c0390100936b3e8be4eafa49356d8bd2e
fahadpktn <fahadpktn@gmail.com>
2022-06-11 20:43:00 +0530

    login form setup

--- sample/login/urls.py
+++ sample/login/urls.py
@@ -3,7 +3,9 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("",views.index,name="templates/index")
+    path("",views.index,name="templates/index"),
+    path("login",views.login_user,name="templates/login"),
+    path("register",views.register,name="templates/register")
 ]
 
 
